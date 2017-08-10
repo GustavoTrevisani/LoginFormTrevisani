f1b6b330f22c6304445fcce648459b302e98c38f
Gustavo Trevisani <gu_trevisani@yahoo.com.br>
2017-08-10 18:09:26 -0300

    Edit Task Running

--- src/main/java/com/loginform/controller/FakeController.java
+++ src/main/java/com/loginform/controller/FakeController.java
@@ -1,5 +1,8 @@
 package com.loginform.controller;
 
+import java.time.Instant;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +22,13 @@ public class FakeController {
 	
 	@GetMapping("/database")
 	public String createFakeDatabase() {
-//		User dev = userRepo.save(new User("1", "1", "Dev", "01/01/2000", "Male", "Av. Everywhere,0000"));
-//		Task task1 = taskRepo.save(new Task("Task1", "01/01/2001", "02/01/2001", dev));
-//		Task task2 = taskRepo.save(new Task("Task2", "01/02/2002", "02/02/2002", dev));
-//		dev.getTasks().add(task1);
-//		dev.getTasks().add(task2);
+		Date data = new Date();
+		data = Date.from(Instant.now());		
+		User dev = userRepo.save(new User("1", "1", "Dev", data, "Male", "Av. Everywhere,0000"));
+		Task task1 = taskRepo.save(new Task("Task1", data, data, dev));
+		Task task2 = taskRepo.save(new Task("Task2", data, data , dev));
+		dev.getTasks().add(task1);
+		dev.getTasks().add(task2);
 		return "redirect:/";
 		
 	}
--- src/main/java/com/loginform/controller/TaskController.java
+++ src/main/java/com/loginform/controller/TaskController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.loginform.bean.Task;
 import com.loginform.bean.User;
@@ -33,10 +32,47 @@ public class TaskController {
 		model.addAttribute("loggedUser", loggedUser);
 		return "views/create_task_page";
 	}
+	@GetMapping("/edit-task-request-action")
+	public String editTaskPage(@RequestParam("id") String id,HttpSession session, Model model) {
+		User loggedUser = (User) session.getAttribute("loggedUser");
+		Long taskId = Long.parseLong(id);
+		Task task = taskRepo.findOne(taskId);
+		String beginDate = task.getBeginDate().toString();
+		String endDate = task.getEndDate().toString();
+		model.addAttribute("id", id);
+		model.addAttribute("beginDate", beginDate);
+		model.addAttribute("endDate", endDate);
+		model.addAttribute("loggedUser", loggedUser);
+		model.addAttribute("task", task);
+		return "views/edit_task_page";
+	}
+	@PostMapping("/edit-task-request-action")
+	public String editTaskRequest(@RequestParam("id") String id, @RequestParam("name") String name, @RequestParam("beginDate") String beginDate, @RequestParam("endDate") String endDate, HttpSession session, Model model) {
+		User loggedUser = (User) session.getAttribute("loggedUser");
+		Long taskId = Long.parseLong(id);
+		Task task = taskRepo.findOne(taskId);			
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateOfBegin = null;
+		Date dateOfEnd = null;
+		try {
+			dateOfBegin = formatter.parse(beginDate);
+			dateOfEnd = formatter.parse(endDate);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}			
+		task.setBeginDate(dateOfBegin);
+		task.setEndDate(dateOfEnd);
+		task.setName(name);
+		taskRepo.save(task);
+		loggedUser.getTasks().clear();
+		loggedUser.getTasks().addAll(taskRepo.findByUserId(loggedUser.getId()));		
+		return "redirect:/user/user-home-action";
+	}
 
 	@PostMapping("/create-task-request-action")
 	public String createTask(@RequestParam("name") String name, @RequestParam("beginDate") String beginDate, @RequestParam("endDate") String endDate,
-			Model model, RedirectAttributes redAtri, HttpSession session) {
+			Model model, HttpSession session) {
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		Date dateOfBegin = null;
 		Date dateOfEnd = null;
@@ -49,7 +85,6 @@ public class TaskController {
 		}			
 		User loggedUser = (User) session.getAttribute("loggedUser");
 		loggedUser.getTasks().add(taskRepo.save(new Task(name, dateOfBegin, dateOfEnd, loggedUser)));
-		System.out.println(dateOfBegin.toString());
-		return "redirect:/user/user-home-action";
+				return "redirect:/user/user-home-action";
 	}
 }
